051ffb5339729a13c28d537b236b987715312d32
toton333 <toton333@yahoo.com>
2014-10-29 19:30:04 +0530

    initial product setup

--- application/modules/cartSystem/controllers/CartSystem.php
+++ application/modules/cartSystem/controllers/CartSystem.php
@@ -21,6 +21,43 @@ class CartSystem extends MX_Controller {
 	{
 	  	
 
+     $data['products'] = array(
+						     	array(
+						     		'id' => 'sku_111',
+						     		'name' => 'T-Shirt',
+						     		'qty' => 1,
+						     		'price' => 10.00,
+						     		'options' => array('Size' => 'Triple x', 'Color'=> ' Dark Blue') 
+
+
+						     		),
+						     	array(
+						     		'id' => 'sku_333',
+						     		'name' => 'Shoe',
+						     		'qty' => 1,
+						     		'price' => 20.90
+
+						     		),
+						     	array(
+						     		'id' => 'sku_444',
+						     		'name' => 'Pant',
+						     		'qty' => 1,
+						     		'price' => 60 ,
+						     		'options' => array('Size' => 'Double x', 'Color'=> ' Silver')
+
+
+						     		),
+						     	array(
+						     		'id' => 'sku_666',
+						     		'name' => 'lapop',
+						     		'qty' => 1,
+						     		'price' => 399.99
+
+						     		)
+						     	);
+
+
+
       $data['title'] = 'Shopping Cart';
       $data['module'] = 'CartSystem';
       $data['content'] = 'Cart_content';
--- application/modules/cartSystem/views/Cart_content.php
+++ application/modules/cartSystem/views/Cart_content.php
@@ -1,1 +1,38 @@
-<h1>Products</h1>
+<h3>Products</h3>
+<table width = "100%" border = "1"   >
+
+<tr>
+ <td width="37%" > <strong>ID</strong> </td>
+ <td width="30%" > <strong>NAME</strong></td>
+ <td width="16%" > <strong>PRICE</strong></td>
+ <td width="16%" >&nbsp;</td>
+</tr>
+
+
+	<?php $product_index = 0; ?>
+	<?php foreach($products as $product): ?>
+
+	<tr>
+		
+		<td><?php echo $product['id'] ?></td>
+		<td>
+			<?php
+			 echo $product['name'] ;
+			 if (array_key_exists('options', $product)) {
+			 	echo '<hr/>';
+			 	foreach ($product['options'] as $option_name => $option_value) {
+			 		echo '<i>' . $option_name . ' :</i> ' . $option_value . '<br/>' ;
+			 	}
+			 }
+			 ?>
+		</td>
+		<td><?php echo '$'.$product['price'] ?></td>
+		<td><a href="<?php base_url() ?> cartSystem?id=<?php echo $product_index; ?> ">Add To Cart</a></td>
+	</tr>
+    <?php $product_index ++ ; ?>
+    <?php endforeach; ?>
+
+
+
+
+</table>
